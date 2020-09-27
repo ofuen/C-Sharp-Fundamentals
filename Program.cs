5eefb0903009facc41e09cc4d73824e7d7eeafda
oscar <oscar@o2webdev.com>
2020-09-27 08:46:35 -0400

    Added a new class call Car

--- Program.cs
+++ Program.cs
@@ -8,7 +8,16 @@ namespace C_Sharp_Fundamentals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        
     }
 }
