367e2ab08a51d4213d02daa7e632d72f4248bcdd
oscar <oscar@o2webdev.com>
2020-09-27 08:34:48 -0400

    Added some comments

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 
-// TODO: Work  on creating another class @ofuen
+// TODO: #1 Work  on creating another class @ofuen
 namespace C_Sharp_Fundamentals
 {
+    // TODO: Create another class @ofuen look at issue #1
     class Program
     {
         static void Main(string[] args)
